6380aa6b27626ded7a9ffc9cf255d8d23b898a95
likelion_ksj <rhj4862@gmail.com>
2016-08-03 21:39:40 +0900

    Rollback

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,24 @@
 Rails.application.routes.draw do
+<<<<<<< HEAD
+  root 'home#index'
+  
+  get '/' => 'home#index'
+  get '/application' => 'home#application'
+  get '/main' => 'home#main'
+  get '/calendar' => 'home#calendar'
+  get '/community' => 'home#community'
+  get '/book' => 'home#book'
+  get '/search' => 'home#search'
+=======
+<<<<<<< HEAD
+  root "home#index"
+  get 'home/index'
+
+=======
+  devise_for :users
+>>>>>>> refs/remotes/origin/master
+>>>>>>> origin/noodi
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+ 
 end
